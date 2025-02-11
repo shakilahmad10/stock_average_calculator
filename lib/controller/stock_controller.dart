78bd2b43552f94e00c6aa245b930de608716e492
Shakil Ahmad <shakilahmad323@gmail.com>
2025-02-11 16:04:03 +0530

    singlechildscroll view and stringfixedas(20)

--- lib/controller/stock_controller.dart
+++ lib/controller/stock_controller.dart
@@ -3,25 +3,18 @@ import 'package:get/state_manager.dart';
 class StockController extends GetxController {
   RxDouble averagePrice = 0.0.obs;
   RxInt totalUnits = 0.obs;
+  RxDouble firstPurchase = 0.0.obs;
+  RxDouble secondPurchase = 0.0.obs;
   RxDouble totalAmount = 0.0.obs;
+  RxBool isVisible = false.obs;
+  RxDouble priceDifference = 0.0.obs;
 
   void calculateAverage(int q1, double p1, int q2, double p2) {
-    // print("$q1, $p1, $q2, $p2 ...................calculating");
-    // var totalQty = q1 + q2;
-    // var totalPrice = p1 + p2;
-    // totalUnits.value = totalQty;
-    // totalAmount.value = totalPrice;
-
-    // print(totalUnits.value);
-    // print(totalAmount.value);
-
-    // if (q1 > 0 && p1 > 0 && q2 > 0 && p2 > 0) {
-    //   averagePrice.value = ((q1 * p1) + (q2 * p2)) / (q1 + q2);
-    // } else {
-    //   averagePrice.value = 0.0; // Default value if input is invalid
-    // }
-
+    isVisible.value = true;
     if (q1 > 0 && p1 > 0 && q2 > 0 && p2 > 0) {
+      firstPurchase.value = q1 * p1;
+      secondPurchase.value = q2 * p2;
+      priceDifference.value = p1 - p2;
       totalUnits.value = (q1 + q2).toInt();
       totalAmount.value = (q1 * p1) + (q2 * p2);
       averagePrice.value = totalAmount.value / totalUnits.value;
@@ -36,5 +29,6 @@ class StockController extends GetxController {
     averagePrice.value = 0.0;
     totalAmount.value = 0.0;
     totalUnits.value = 0;
+    isVisible.value = false;
   }
 }
--- lib/views/stock_calculator_screen.dart
+++ lib/views/stock_calculator_screen.dart
@@ -19,149 +19,193 @@ class StockCalculatorScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Stock Average Calculator"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "First purchage",
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-            ),
-            const SizedBox(height: 10),
-            _buildInputField(
-                "Units", "Enter quantity", firstQuantityController),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "First purchase",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+              ),
+              const SizedBox(height: 10),
+              _buildInputField(
+                  "Units", "Enter quantity", firstQuantityController),
 
-            const SizedBox(height: 10),
-            _buildInputField("Price per Share", "Enter the price per share",
-                firstPriceController),
-            const SizedBox(height: 20),
-            //second---------------------------------------------------
-            Text(
-              "Second purchage",
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-            ),
-            const SizedBox(height: 10),
-            _buildInputField(
-                "Units", "Enter quantity", secondQuantityController),
-            const SizedBox(height: 10),
-            _buildInputField("Price per Share", "Enter the price per share",
-                secondPriceController),
-            const SizedBox(height: 20),
+              const SizedBox(height: 10),
+              _buildInputField("Price per Share", "Enter the price per share",
+                  firstPriceController),
 
-            //button row
-            Row(
-              children: [
-                //elevated button
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("clearing the fields");
-                      firstQuantityController.clear();
-                      firstPriceController.clear();
-                      secondQuantityController.clear();
-                      secondPriceController.clear();
+              Obx(() {
+                if (stockController.isVisible.value) {
+                  return _investmentAmountDisplay("First Investment",
+                      stockController.firstPurchase.value.toStringAsFixed(2));
+                } else {
+                  return SizedBox.shrink();
+                }
+              }),
+              const SizedBox(height: 15),
+              //second---------------------------------------------------
+              Text(
+                "Second purchase",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+              ),
+              const SizedBox(height: 10),
+              _buildInputField(
+                  "Units", "Enter quantity", secondQuantityController),
+              const SizedBox(height: 10),
+              _buildInputField("Price per Share", "Enter the price per share",
+                  secondPriceController),
+              Obx(() {
+                if (stockController.isVisible.value) {
+                  return _investmentAmountDisplay("Second Investment",
+                      stockController.secondPurchase.value.toStringAsFixed(2));
+                } else {
+                  return SizedBox.shrink();
+                }
+              }),
+              const SizedBox(height: 20),
 
-                      stockController.resetValues();
-                    },
-                    style: _buildButtonStyle(Colors.amber),
-                    child: Text(
-                      "Clear Fields",
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Calculating the average....");
-                      final firstQty = firstQuantityController.text;
-                      final firstPrice = firstPriceController.text;
-                      final secondQty = secondQuantityController.text;
-                      final secondPrice = secondPriceController.text;
-
-                      if (firstQty.isEmpty ||
-                          firstPrice.isEmpty ||
-                          secondQty.isEmpty ||
-                          secondPrice.isEmpty) {
-                        //show message
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Please fill all fields."),
-                          backgroundColor: Colors.red,
-                        ));
-                        return;
-                      }
-
-                      //input parse to double
-                      final int q1 = int.tryParse(firstQty) ?? 0;
-                      final double p1 = double.tryParse(firstPrice) ?? 0;
-                      final int q2 = int.tryParse(secondQty) ?? 0;
-                      final double p2 = double.tryParse(secondPrice) ?? 0;
-
-                      if (q1 <= 0 || p1 <= 0 || q2 <= 0 || p2 <= 0) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Please enter valid positive numbers."),
-                          backgroundColor: Colors.red,
-                        ));
-                        return;
-                      }
-
-                      stockController.calculateAverage(q1, p1, q2, p2);
-                    },
-                    style: _buildButtonStyle(Colors.green),
-                    child: Text(
-                      "Calculate Average",
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            //Diplay calculated Average value
-            SizedBox(
-              height: 20,
-            ),
-            Obx(() {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              //button row
+              Row(
                 children: [
-                  _customTextDisplay("Total Units: ",
-                      stockController.totalUnits.value.toString()),
-                  _customTextDisplay("Average Price: ",
-                      stockController.averagePrice.value.toStringAsFixed(2)),
-                  _customTextDisplay("Total Amount: ",
-                      stockController.totalAmount.value.toStringAsFixed(2)),
+                  //elevated button
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        firstQuantityController.clear();
+                        firstPriceController.clear();
+                        secondQuantityController.clear();
+                        secondPriceController.clear();
+
+                        stockController.resetValues();
+                      },
+                      style: _buildButtonStyle(Colors.amber),
+                      child: Text(
+                        "Clear Fields",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Calculating the average....");
+                        final firstQty = firstQuantityController.text;
+                        final firstPrice = firstPriceController.text;
+                        final secondQty = secondQuantityController.text;
+                        final secondPrice = secondPriceController.text;
+
+                        if (firstQty.isEmpty ||
+                            firstPrice.isEmpty ||
+                            secondQty.isEmpty ||
+                            secondPrice.isEmpty) {
+                          //show message
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Please fill all fields."),
+                            backgroundColor: Colors.red,
+                          ));
+                          return;
+                        }
+
+                        //input parse to double
+                        final int q1 = int.tryParse(firstQty) ?? 0;
+                        final double p1 = double.tryParse(firstPrice) ?? 0;
+                        final int q2 = int.tryParse(secondQty) ?? 0;
+                        final double p2 = double.tryParse(secondPrice) ?? 0;
+
+                        if (q1 <= 0 || p1 <= 0 || q2 <= 0 || p2 <= 0) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content:
+                                Text("Please enter valid positive numbers."),
+                            backgroundColor: Colors.red,
+                          ));
+                          return;
+                        }
+
+                        stockController.calculateAverage(q1, p1, q2, p2);
+                      },
+                      style: _buildButtonStyle(Colors.green),
+                      child: Text(
+                        "Calculate Average",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-              );
-            }),
-          ],
+              ),
+
+              //Diplay calculated Average value
+              SizedBox(
+                height: 20,
+              ),
+              Obx(() {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _customTextDisplay("Total Units: ",
+                        stockController.totalUnits.value.toString(), 18),
+                    _customTextDisplay(
+                        "Average Price: ",
+                        stockController.averagePrice.value.toStringAsFixed(2),
+                        18),
+                    _customTextDisplay(
+                        "Total Amount: ",
+                        stockController.totalAmount.value.toStringAsFixed(2),
+                        18),
+                    _customTextDisplay(
+                        "Share Price Difference: ",
+                        stockController.priceDifference.value
+                            .toStringAsFixed(2),
+                        18),
+                  ],
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _customTextDisplay(String label, String value) {
-    print("$label $value"); // Log label and value for debugging
+  Widget _investmentAmountDisplay(String label, String amount) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          label,
+          style: TextStyle(color: const Color.fromARGB(255, 1, 131, 6)),
+        ),
+        const SizedBox(
+          width: 6,
+        ),
+        Text(
+          amount,
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
 
+  Widget _customTextDisplay(String label, String value, double fontSize) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: Row(
-        mainAxisAlignment:
-            MainAxisAlignment.spaceBetween, // Align text in a straight column
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             label,
-            style: const TextStyle(
-              fontSize: 18,
+            style: TextStyle(
+              fontSize: fontSize,
               fontWeight: FontWeight.w400,
               color: Color.fromARGB(255, 13, 136, 17),
             ),
